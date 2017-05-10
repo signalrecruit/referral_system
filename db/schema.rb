9ca4b777ebe2f5aa20419251cdf26446418bc33c
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-05-10 23:33:56 +0000

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170510111153) do
+ActiveRecord::Schema.define(version: 20170510233300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20170510111153) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "status",             default: "none"
+    t.decimal  "earnings",           default: 0.0
   end
 
   add_index "applicants", ["company_id"], name: "index_applicants_on_company_id", using: :btree
