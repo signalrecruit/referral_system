7d2a07b8e88c25333d7c6edd3cacbf5498ea7869
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-05-12 10:44:36 +0000

    move display analytics method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,4 +46,37 @@ class ApplicationController < ActionController::Base
   def create_notification(action, notification_type)
     Notification.create! action: action, notification_type: notification_type  
   end
+
+   def display_analytics
+    total_revenue
+    total_number_of_hired_applicants
+    total_number_of_company_deals
+    top_earner
+  end
+
+  def total_revenue
+    @completed_jds = []
+    JobDescription.all.each do |jd|
+      if jd.applicants.all? { |applicant| applicant.hired? }
+        @completed_jds << jd.worth
+      end 
+      @total_revenue = @completed_jds.inject(0){ |sum, x| sum + x }
+    end 
+  end
+
+  def total_number_of_hired_applicants
+    @number_of_hired_applicants = Applicant.all.where(status: "hired").count     
+  end
+
+  def total_number_of_company_deals
+    @number_of_deals = Company.all.where(deal: true).count
+  end
+
+  def top_earner
+    if User.any? { |user| user.cumulative_earnings != 0 } 
+      @top_earner = User.all.order(cumulative_earnings: :desc).first
+    else 
+      @top_earner = nil 
+    end
+  end
 end
