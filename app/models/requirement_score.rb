df2d5c42789c5b4ddb84adbb63b239b108668192
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-06-07 19:30:51 +0000

    commit changes

--- app/models/requirement_score.rb
+++ app/models/requirement_score.rb
@@ -2,4 +2,11 @@ class RequirementScore < ActiveRecord::Base
   belongs_to :applicant
   belongs_to :job_description
   belongs_to :requirement
+
+  after_save :calculate_applicant_score
+
+
+  def calculate_applicant_score
+   	
+  end
 end
