ed7fbcbb7564831910bb12bd249d89ad49c16741
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-06-24 00:07:43 +0000

    commit changes to error_handler.rb

--- lib/error/error_handler.rb
+++ lib/error/error_handler.rb
@@ -2,8 +2,9 @@ module Error
   module ErrorHandler
     def self.included base 
       base.class_eval do 
+      	rescue_from StandardError, with: :unknown_error
         rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-        rescue_from ActionController::RoutingError, with: :routing_error
+        # rescue_from ActionController::RoutingError, with: :routing_error
       end
     end
 
@@ -18,14 +19,14 @@ module Error
 
     private 
 
-    # def rescue_all_errors
-    #   flash[:alert] = "oops! something went wrong."
-    #   if current_user.admin?
-    #   	redirect_to admin_dashboard_url
-    #   else	
-    #     redirect_to root_url
-    #   end    	
-    # end
+    def unknown_error(exception)
+      flash[:alert] = "oops! something went wrong."
+      if current_user.admin?
+      	redirect_to admin_dashboard_url
+      else	
+        redirect_to root_url
+      end    	
+    end
 
     def record_not_found
       flash[:alert] = "the resource you are looking for doesn't exist."
