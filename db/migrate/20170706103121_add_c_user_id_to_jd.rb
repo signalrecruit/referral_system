553fb1ef3a9affea7b002c24895ba4217c5ba02a
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-07-06 11:56:07 +0000

    fix migration issues

--- db/migrate/20170706103121_add_c_user_id_to_jd.rb
+++ db/migrate/20170706103121_add_c_user_id_to_jd.rb
@@ -1,5 +1,5 @@
 class AddCUserIdToJd < ActiveRecord::Migration
   def change
-  	add_column :job_descriptions, :c_user_id, :integer, default: nil #edit_user_id is the id of current_user editing jd
+  	add_column :job_descriptions, :edit_user_id, :integer, default: nil #edit_user_id is the id of current_user editing jd
   end
 end
