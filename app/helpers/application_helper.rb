9f4247f4ef6f508e0d5604615fbc291a09bcca86
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-07-27 20:22:27 +0000

    created a helper method to handle the effect of deleted resource on notification

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def if_resource_exists? notification
+    if find_resource notification
+      yield 	
+    end 
+  end
+
+  def find_resource notification
+    (notification.resource_type.constantize).find(notification.resource_id)
+rescue ActiveRecord::RecordNotFound 
+	false
+  end
 end
