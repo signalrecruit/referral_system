f0d5806f0362a9285ac49e7e1b9a7d28fc962844
Derek Owusu-Frimpong <deepsky_5@live.com>
2017-08-10 13:12:43 +0000

    commit changes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
 
   def index
   	display_analytics
-  	if current_user && current_user.bank_accounts.empty? 
+  	if current_user && (current_user.bank_accounts.empty? && current_user.payment_option == "bank") 
   	  flash.now[:warning] = "Hello, #{current_user.fullname}. Here is a reminder to fill out your bank account details." 
   	end
   end
