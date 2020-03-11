9ed559ce2f8e9cf8b2925e8759076d4c0cc4b535
Derek Owusu-Frimpong <deepsky_5@live.com>
2020-03-11 00:09:24 +0000

    Update activity_feed_controller.rb

--- app/controllers/admin/activity_feed_controller.rb
+++ app/controllers/admin/activity_feed_controller.rb
@@ -3,7 +3,6 @@ class Admin::ActivityFeedController < Admin::ApplicationController
 
   def index
   	@feed = Activity.order(created_at: :desc)
-  	# fresh_when last_modified: @feed.maximum(:updated_at)
   end
 
   def show
